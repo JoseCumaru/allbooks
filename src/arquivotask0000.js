360ab6b364a0c8166b9f862567ec11b62fb8e1dd
Jose Neto <josecumaru@bemol.com.br>
2025-08-22 00:25:54 -0400

    Adiciona função de teste simples

---
+++ src/arquivotask0000.js
@@ -0,0 +1,8 @@
+// Simple test function
+function teste() {
+    console.log("Função de teste executada");
+    return true;
+}
+
+// Export the function so it can be used in other files
+module.exports = teste;
